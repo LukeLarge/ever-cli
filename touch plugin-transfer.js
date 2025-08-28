2e4e5439e53dc197dcb28a636bd290f9feaaf33d
LukeLarge <162439255+LukeLarge@users.noreply.github.com>
2025-08-28 17:35:28 -0500

    transfer

---
+++ touch plugin-transfer.js
@@ -0,0 +1,6 @@
+const TonWeb = require('tonweb');
+
+// Replace 'YOUR_NEW_API_KEY' with your actual TON API key
+const tonweb = new TonWeb(new TonWeb.HttpProvider('https://toncenter.com/api/v2/jsonRPC', { apiKey: '6c9d64e8de2e844bab76223d884d4b6af2897b2c8b8b9faf66a10f640f779718'
+	
+ }));
